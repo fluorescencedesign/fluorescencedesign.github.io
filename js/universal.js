b72735530e894a5c4a4601e2c66c4a7148333cd4
richardj-lin <richardlin4375@gmail.com>
2025-05-30 09:10:44 -0400

    Finished website

---
+++ js/universal.js
@@ -0,0 +1,25 @@
+var offset = 100;
+function contactScroll() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+function aboutScroll() {
+    scrollTo({top: document.getElementById("about").offsetTop - offset, behavior: "smooth"});
+}
+function topScroll() {
+    scrollTo({top: 0, behavior: "smooth"});
+}
+
+function openPortfolio() {
+    window.open("https://richardj-lin.github.io/")
+}
+function openPortfolio1() {
+    window.open("https://tsapulsar.github.io/")
+}
+function openPortfolio2() {
+    window.open("https://youthhealthymind.com/")
+}
+function openPortfolio3() {
+    window.open("https://thepowcenter.com/")
+}
